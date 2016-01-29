69e40aaef5f8228a349e7af00ad63aff32b8773f
crisgh23 <cristina_gh2.3@hotmail.com>
2016-01-29 11:23:23 +0100

    Update tablas.py

--- tablas.py
+++ tablas.py
@@ -2,7 +2,7 @@
 #-*- coding: utf-8 -*-
 numeros = range(1,11)
 for elemento1 in numeros:
-    print "/nTabla del ",str(elemento1)
+    print "\nTabla del " + str(elemento1)
     print "------------"
     for elemento2 in numeros:
         print str(elemento1) + " por " + str(elemento2) + " = " + str(elemento1*elemento2)
